481706e74ac54a6994267a81d0db95aab72505ad
jaiminmungalpara <47455324+jaiminmungalpara@users.noreply.github.com>
2023-07-25 15:19:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -80,6 +80,11 @@ if __name__ == "__main__":
 
         ## For Remote server only(DAGShub)
 
+        remote_server_uri="https://dagshub.com/jaiminmungalpara/mlflow.mlflow"
+        mlflow.set_tracking_uri(remote_server_uri)
+
+        ## For Remote server only(DAGShub)
+
         remote_server_uri="https://dagshub.com/krishnaik06/mlflowexperiments.mlflow"
         mlflow.set_tracking_uri(remote_server_uri)
 
